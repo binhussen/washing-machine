3493f74b4a575cb54ea609db4f7c2c09383d3715
binhussen <mhdhsna@gmail.com>
2022-10-26 21:52:40 +0300

    Add Interface

---
+++ API/Interface/IMachine.cs
@@ -0,0 +1,13 @@
+﻿using API.Entity;
+
+namespace API.Interface
+{
+    public interface IMachine
+    {
+        IEnumerable<Machine> GetAll();
+        Machine GetById(int id);
+        void Create(Machine time);
+        void Update(int id, Machine time);
+        void Delete(int id);
+    }
+}
---
+++ API/Interface/ITime.cs
@@ -0,0 +1,13 @@
+﻿using API.Entity;
+
+namespace API.Interface
+{
+    public interface ITime
+    {
+        IEnumerable<Time> GetAll(int machineId);
+        Time GetById(int machineId,int id);
+        void Create(int machineId,Time time);
+        void Update(int machineId,int id, Time time);
+        void Delete(int machineId,int id);
+    }
+}
