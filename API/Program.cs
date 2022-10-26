53d0a3ea5a839c57435745f3c6223cec65487d03
binhussen <mhdhsna@gmail.com>
2022-10-26 21:54:15 +0300

    Add Context

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<DataContext>();
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
