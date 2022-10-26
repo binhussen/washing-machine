64a4686987d86b344b6ea36ae8886896a62681ee
binhussen <mhdhsna@gmail.com>
2022-10-26 21:54:15 +0300

    Add Context

---
+++ API/DataContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+using API.Entity;
+using Microsoft.EntityFrameworkCore;
+
+public class DataContext : DbContext
+{
+    protected readonly IConfiguration _config;
+
+    public DataContext(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        options.UseInMemoryDatabase("MachineDatabase");
+    }
+
+    public DbSet<Time> Times { get; set; }
+    public DbSet<Machine> Machines { get; set; }
+}
