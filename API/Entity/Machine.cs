5c31a0ad9825d689107eeba42b48a3d08654f7cf
binhussen <mhdhsna@gmail.com>
2022-10-26 21:52:22 +0300

    Add Models

---
+++ API/Entity/Machine.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entity
+{
+    public class Machine
+    {
+        [Column("MachineId")]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public ICollection<Time> Times { get; set; }
+    }
+}
