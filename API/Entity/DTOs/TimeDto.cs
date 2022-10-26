182b14be7cd711b5f4ab5f30666e730aa5d696b1
binhussen <mhdhsna@gmail.com>
2022-10-26 21:53:41 +0300

    Add Dto

---
+++ API/Entity/DTOs/TimeDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entity.DTOs
+{
+    public class TimeDto
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool Status { get; set; }
+        public int MachineId { get; set; }
+    }
+}
