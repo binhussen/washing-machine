380939dff472ea02a69a5d54009da1ce5f063009
binhussen <mhdhsna@gmail.com>
2022-10-26 21:52:22 +0300

    Add Models

---
+++ API/Entity/Time.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Entity
+{
+    public class Time
+    {
+        public int Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool Status { get; set; }
+        [ForeignKey(nameof(Machine))]
+        public int MachineId { get; set; }
+    }
+}
