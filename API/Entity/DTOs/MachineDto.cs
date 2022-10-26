f714fc5f001234a68f23502fbfd1ceb88c513c79
binhussen <mhdhsna@gmail.com>
2022-10-26 21:53:41 +0300

    Add Dto

---
+++ API/Entity/DTOs/MachineDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Entity.DTOs
+{
+    public class MachineDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IEnumerable<TimeDto>? Times { get; set; }
+    }
+}
